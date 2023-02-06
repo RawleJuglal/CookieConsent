c4e0a82fe4ef84da6b6079fa97b497835db8bfb6
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-02-05 23:45:35 -0600

    completed app
    Finished modal application

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import './style.css'
 const modalEl = document.getElementById('modal')
 const modalCloseEl = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalTextEl = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceContainer = document.getElementById('modal-choice-btns')
+
 
 setTimeout(function(){
     modalEl.style.display = 'inline'
@@ -12,7 +16,33 @@ modalCloseEl.addEventListener('click', ()=>{
   modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseover', ()=>{
+  modalChoiceContainer.classList.toggle('reverse')
+})
+
 consentForm.addEventListener('submit', (e)=>{
   e.preventDefault()
-  console.log('form submitted')
+  const consentFormData = new FormData(consentForm)
+  let name = consentFormData.get('fullName')
+
+  modalTextEl.innerHTML = `<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>`
+
+    setTimeout(()=>{
+      document.getElementById('uploadText').textContent = "Making the sale..."
+      setTimeout(function(){
+          document.getElementById('modal-inner').innerHTML = `
+          <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+          <p>We just sold the rights to your eternal soul.</p>
+          <div class="idiot-gif">
+              <img src="images/pirate.gif">
+          </div>
+      `
+      modalCloseEl.disabled = false;
+      }, 1500)
+    }, 1500)
 })
