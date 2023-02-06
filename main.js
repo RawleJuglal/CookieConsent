0a2a1b9882e24fb8f64e182bc6317b5fd1fcb990
Rawle Juglal <rawledjuglal@gmail.com>
2023-02-05 20:45:37 -0600

    saving progress
    modal and submit buttons

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const modalEl = document.getElementById('modal')
+const modalCloseEl = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+
+setTimeout(function(){
+    modalEl.style.display = 'inline'
+}, 1500)
+
+modalCloseEl.addEventListener('click', ()=>{
+  modal.style.display = 'none'
+})
+
+consentForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  console.log('form submitted')
+})
